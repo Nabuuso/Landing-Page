7774df5481e7530156eaaadfb95ff1c319f0bf21
betty <betty.nabuuso@moringaschool.com>
2021-09-07 10:51:43 +0300

    more commits

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 alert("AKAN NAMES");
+function computation(){
+    //arrays declaration
+    let malenames=["kwasi", "Kwadwo", "Kwabena", 
+    "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let FemaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+}
