901ac35b3bf2c39f0603c34710eb6e64e71f4603
betty <betty.nabuuso@moringaschool.com>
2021-09-07 18:17:07 +0300

    add a few changes

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ function naming(){
   var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   if(CC.value==""||YY.value==""||MM.value==""||DD.value=="");
+  alert("No blank values allowed");
 }
 var gender = document.querySelector('input[name="gender"]:checked').value;
 var dayOfTheWeek= parseInt(( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
